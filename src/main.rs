04a22dc9991bad8f90ff554429e0deea9da0286b
Mike Bruce <mike@hypalynx.com>
2026-03-23 08:05:28 +0000

    styling input area

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use ratatui::prelude::{Constraint, Direction, Layout};
+use ratatui::style::Stylize;
+use ratatui::widgets::Block;
 use ratatui::{DefaultTerminal, Frame};
 
 fn main() -> anyhow::Result<()> {
@@ -21,6 +23,8 @@ fn render(frame: &mut Frame) {
         .constraints(vec![Constraint::Fill(1), Constraint::Length(5)])
         .split(frame.area());
 
+    let input_block = Block::bordered().white().on_black();
+
     frame.render_widget("hello world", layout[0]);
-    frame.render_widget("what is up", layout[1]);
+    frame.render_widget(input_block, layout[1]);
 }
