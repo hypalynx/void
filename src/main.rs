ce197c1e9b229baeff1d7ab53d9c532950819bb8
Mike Bruce <mike@hypalynx.com>
2026-03-23 20:37:39 +0000

    more styling, cursor shows in input box

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use ratatui::{DefaultTerminal, Frame};
 const HORIZONTAL_MARGIN: u16 = 2;
 const VERTICAL_MARGIN: u16 = 1;
 const SPACING: u16 = 1;
+const HORIZONTAL_INPUT_MARGIN: u16 = 2;
+const VERTICAL_INPUT_MARGIN: u16 = 1;
 
 enum InputCommand {
     InsertChar(char),
@@ -70,21 +72,27 @@ fn render(frame: &mut Frame, input: &str) {
         .direction(Direction::Vertical)
         .constraints(vec![
             Constraint::Fill(1),
-            Constraint::Length(5),
+            Constraint::Length(3),
             Constraint::Length(1),
         ])
         .split(frame.area());
 
-    let input_block = Block::new()
-        .white()
-        .on_black()
-        .padding(Padding::new(2, 2, 1, 1));
+    let input_block = Block::new().white().on_black().padding(Padding::new(
+        HORIZONTAL_INPUT_MARGIN,
+        HORIZONTAL_INPUT_MARGIN,
+        VERTICAL_INPUT_MARGIN,
+        VERTICAL_INPUT_MARGIN,
+    ));
     let inner = input_block.inner(layout[1]);
 
     frame.render_widget("hello world", layout[0]);
     frame.render_widget(input_block, layout[1]);
     frame.render_widget(input, inner);
     frame.render_widget("status", layout[2]);
+
+    let cursor_x = inner.x + input.len() as u16;
+    let cursor_y = inner.y;
+    frame.set_cursor_position((cursor_x, cursor_y));
 }
 
 fn insert(input: &mut String, ch: char) {
